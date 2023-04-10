732be433a8f682797c3ed7e76ece477ce4b4196d
Mathis <44369891+Ommander@users.noreply.github.com>
2023-04-10 13:56:44 +0200

    Kommentare WindowSQLlogin.xaml.cs

--- WindowSQLlogin.xaml.cs
+++ WindowSQLlogin.xaml.cs
@@ -17,23 +17,29 @@ namespace _202223_bbs_projekt_kasse
 {
     /// <summary>
     /// Interaktionslogik für WindowSQLlogin.xaml
+    /// Fenster zur erneuten bestimmung der Login Parameter für SQL Verbindung, bei gescheiterter Verbindungsaufnahme zur Datenbank
     /// </summary>
     public partial class WindowSQLlogin : Window
     {
         public WindowSQLlogin()
         {
             InitializeComponent();
+            //Übernahme der Login Parameter zur erneuten Anzeige der zuerst eingegebenen Wert
             sqliplogin.Text = GLOBALS.SQL_IP;
             sqluserlogin.Text = GLOBALS.SQL_USER;
             sqlpasslogin.Password = GLOBALS.SQL_PASSWORD;
             sqldblogin.Text = GLOBALS.SQL_DB;
         }
+
+        //Funktionalität Confirm Knopf
         private void sqlloginconfirm_Click(object sender, RoutedEventArgs e)
         {
+            //Geänderte Variabeln werden wieder in Globale Variablen Geschreiben
             GLOBALS.SQL_IP = sqliplogin.Text;
             GLOBALS.SQL_USER = sqluserlogin.Text;
             GLOBALS.SQL_PASSWORD = sqlpasslogin.Password;
             GLOBALS.SQL_DB = sqldblogin.Text;
+            //WindowSQLlogin wird geschlossen
             this.Close();
         }
     }
